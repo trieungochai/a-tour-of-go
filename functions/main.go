114e173be4e0cf11d4644d0699f2b32bf3542691
trieungochai <trieungochai.dev@gmail.com>
2024-08-04 00:39:00 +0700

    functions

---
+++ functions/main.go
@@ -0,0 +1,16 @@
+// ## Functions
+// A function can take zero or more arguments.
+// In this example, add takes two parameters of type int.
+// Notice that the type comes after the variable name.
+
+package main
+
+import "fmt"
+
+func add(x int, y int) int {
+	return x + y
+}
+
+func main() {
+	fmt.Println(add(6, 9))
+}
