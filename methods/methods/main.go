15306592b9c635fcba7e636cc3f680d6fcc3aa0c
trieungochai <trieungochai.dev@gmail.com>
2024-08-11 19:04:34 +0700

    methods are functions

--- methods/methods/main.go
+++ methods/methods/main.go
@@ -14,11 +14,19 @@ type Vertex struct {
 	X, Y float64
 }
 
-func (v Vertex) Abs() float64 {
+// func (v Vertex) Abs() float64 {
+// 	return math.Sqrt(v.X*v.X + v.Y*v.Y)
+// }
+
+// ##Methods are functions (https://go.dev/tour/methods/2)
+// Remember: a method is just a function with a receiver argument.
+// Here's Abs written as a regular function with no change in functionality.
+func Abs(v Vertex) float64 {
 	return math.Sqrt(v.X*v.X + v.Y*v.Y)
 }
 
 func main() {
 	v := Vertex{3, 4}
-	fmt.Println(v.Abs())
+	// fmt.Println(v.Abs())
+	fmt.Println(Abs(v))
 }
